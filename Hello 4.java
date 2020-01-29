08e1d2090c5029877fce90358dfff4e6a9162b8b
zwallace555 <60410756+zwallace555@users.noreply.github.com>
2020-01-29 09:13:26 -0600

    Add files via upload

--- Hello 4.java
+++ Hello 4.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 class Hello
 {
-	public static void main ( String[] args )
+	public static void main ( String[] args)
 	{
-		System.out.println( "Hello World! :D" ) ;
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println( "Hello Neighbor! :D" ) ;
+		System.out.print("What is your name?  ");
+			
+		String name = userInput.nextLine();
+		if(name.equals("Zach")){
+			System.out.println("BEEP BOOP BEEP");
+			System.out.println("\007");
+			}
+			else
+			System.out.println("DDOS INCOMING " + name + "! My name is Zach!" +
+				" Here's a cookie! 01000011 01101111 01101111 01101011 01101001 01100101");
+						
+				
 	}
-
+	public void MakeADecision(int A){
+		System.out(A);
+	}
+	
 }
