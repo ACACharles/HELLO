1261ea7c275cc444e2eff3f63369e0573c251f06
PhilMcConn <60410681+PhilMcConn@users.noreply.github.com>
2020-01-29 08:42:19 -0600

    Add files via upload

--- Hello5.java
+++ Hello5.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 class Hello5
 {
 	public static void main (String[] args)
 	{
+		Scanner input = new Scanner(System.in);
 		System.out.println("Hello World!");
-	}
+		System.out.println();
+		System.out.println("What is your name? ");
+		String name = input.nextLine();
+		
+		if(name.equals("Philip")){
+			System.out.println("Oh wow thats my name too!!!!");
+		}
+		else 
+			System.out.println("Hello " + name + " nice to meet you.");
+		}
+		
+		void decision(int d)
+		{
+				System.out.println(d);
+		}
 }
