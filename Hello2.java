1fe97e645aba6ed826c556fb9d2be912bcefe851
Bryce Lane <60410721+blane18@users.noreply.github.com>
2020-01-29 09:25:09 -0600

    Add files via upload

--- Hello2.java
+++ Hello2.java
@@ -1,7 +1,30 @@
-class Hello
+import java.util.Scanner ;
+class Hello2
 {
 	public static void main (String[] args )
 	{
-	System.out.println( "Hello World!" ) ;
+	
+	Scanner UserInput = new Scanner( System.in) ;
+
+	String Name;
+	
+	System.out.print(" What is your name? " ) ;
+	Name = UserInput.next() ;
+	
+	
+	if (Name.equals("Bryce")) 
+	{
+		System.out.print("hello Bryce");
+	}
+	else
+	{
+		System.out.print("hello user");
+	}
+	
+	
+	}
+	void Decision(int a) {
+		System.out.print(a);
 	}
 }
+		
