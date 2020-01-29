4001c610c1b2516000c7912003fed6cda3754fcd
LoganCCarter <60410616+LoganCCarter@users.noreply.github.com>
2020-01-29 09:12:03 -0600

    Add files via upload

--- Hello7.java
+++ Hello7.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Hello7{
 	public static void main(String[]args){
-System.out.println("Hello7");
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("What us your name?");
+		String userName = input.nextLine();
+		
+		if (userName.equals("Logan")){
+			System.out.println("Hello Owner " + userName);
+		}
+		else System.out.println("Hello Guest User " + userName);
+		
+
+	
 }
+void decision(int a){
+		System.out.println(a);
+	}
 }
