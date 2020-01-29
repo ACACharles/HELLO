dfc395ae33e6edb15ee2ccc5f9264e53eb3f6a88
ewalker0512 <60410770+ewalker0512@users.noreply.github.com>
2020-01-29 09:15:07 -0600

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 class Hello
-{
-	public static void main ( String[] args )
 	{
-		System.out.println( "Hello World!" ) ;
+	void decision(int a){
+		System.out.println(a);
+	}
+		
+		public static void main ( String[] args )
+	{
+		int Prince = 1999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("But tonight Im gonna party like its...");
+		if(input.nextInt() == Prince){
+			System.out.println("Access Granted");
+			System.out.println( "Hello World!" );
+		}else{
+			System.out.println("Access Denied");
+			System.out.println("Not a Prince fan huh?");
+		}
+		
 	}
 }
