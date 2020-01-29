cfb0ae6709b6c19af4e47d851ffc6be0373f13c3
AndrewBiggam <60410622+AndrewBiggam@users.noreply.github.com>
2020-01-29 08:54:21 -0600

    Add files via upload

--- Hello8.java
+++ Hello8.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 class Hello8
 {
-public static void main ( String[] args )
-{
-System.out.println( "Hello World 8!(watch out for all the lava and cannons)" ) ;
-}
+	public static void main ( String[] args )
+	{
+		System.out.println("What is your name?");
+		
+		Scanner input = new Scanner(System.in);
+		
+		String name = input.nextLine();
+	
+		if(name.equals("Andrew"))
+		{
+			System.out.println( "Hey! You're not Andrew!!!");
+		}
+	
+		System.out.println( "Hello World 8!(watch out for all the lava and cannons)" ) ;
+	}
+	public static void decision(int a)
+	{
+		//I don't really know what I'm doing here.
+		System.out.println(int a);
+	}
 }
