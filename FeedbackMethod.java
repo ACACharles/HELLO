6cd9f742d5c8fe0128ebc8ab3b6ac5731b50bbc1
BenjaminThomasJones <60410761+BenjaminThomasJones@users.noreply.github.com>
2020-01-29 09:08:41 -0600

    Add files via upload

---
+++ FeedbackMethod.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+class ScannerExample {  
+	public static void main(String args[]){  
+		Scanner in = new Scanner(System.in);  
+		System.out.print("Enter your name: ");  
+		String name = in.nextLine(); 
+		if(name.equals("bingus"))
+		System.out.println("YOU'RE NAME BAD");
+		else System.out.println("Name is: " + name);             
+		in.close();
+	}
+	void ebic(int d){
+	System.out.println(d);
+	}
+}  
