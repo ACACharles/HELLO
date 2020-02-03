ca94139c8c5da85c252f1eb61e56351ff9da78a1
ddodgg <60410605+ddodgg@users.noreply.github.com>
2020-02-03 16:05:34 -0600

    Add files via upload

--- Hello3.java
+++ Hello3.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 class Hello3
 {	 
     public static void main ( String[] args ) {
-	
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("What is your name?: ");
-		String str = userInput.nextLine();
+	int Vaccine;
+	Scanner userInput = new Scanner(System.in);
+	System.out.println("What is your name?: ");
+	String str = userInput.nextLine();
 		
 			if(str.equals("David")) 
 			System.out.println("Hello " + str + " Password Accepted. Welcome To The Matrix."); 
@@ -24,18 +24,28 @@ class Hello3
 			System.out.println("\007");
 			System.out.println(1);
 			Thread.sleep(2000);
-			System.out.println("J/K HELLO " + str +"!");
+			System.out.println("J/K HELLO " + str +" I'll Give You One Shot For The Vaccine. Good Luck.");
         } 
 		catch (InterruptedException ie)
         {
             System.out.println("...");
-				}
-			}
+		}	
+	}
+	
 			
 	public void Decision(int a) {
-		System.out.println(a);
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Pick a number between 1 -5 get it right and you get to live!");
+		a = userInput.nextInt();
+		switch(a) {
+			case 1 : case 2: case 4: case 5: default:
+				System.out.println("Wrong Number No Vaccine For You. Now You Are Dead."); break;
+			case 3:
+				System.out.println("Congratulations You Get To Live Another Day. Here is the Vaccine to the T Virus.");
+		}	
 	}	
 	
+
 	public void Decision(String umbrellaCorp) {
 		System.out.println(umbrellaCorp);
 	}	
@@ -45,3 +55,4 @@ class Hello3
 		System.out.println(raccoonCity);
 	}	
 }
+
