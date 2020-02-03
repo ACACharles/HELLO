5f328b9aac7b2b83d36bd320a903a71ad9954d9d
AndrewBiggam <60410622+AndrewBiggam@users.noreply.github.com>
2020-02-03 15:38:09 -0600

    Add files via upload

--- Hello8.java
+++ Hello8.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
-class Hello8
-{
-	public static void main ( String[] args )
-	{
+
+class Hello8 {
+	public static void main(String[] args) {
 		System.out.println("What is your name?");
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		String name = input.nextLine();
-	
-		if(name.equals("Andrew"))
-		{
-			System.out.println( "Hey! You're not Andrew!!!");
+
+		if (name.equals("Andrew")) {
+			System.out.println("Hey! You're not Andrew!!!");
 		}
-	
-		System.out.println( "Hello World 8!(watch out for all the lava and cannons)" ) ;
+
+		System.out.println("Hello World 8!(watch out for all the lava and cannons)");
 	}
+
 	public static void decision(int a)
 	{
-		//I don't really know what I'm doing here.
-		System.out.println(a);
+		switch(a)
+		{
+			case 1: System.out.print("This is case 1"); break;
+			case 2: System.out.print("This is case 2"); break;
+			case 3: System.out.print("This is case 3"); break;
+			case 4: System.out.print("This is case 4"); break;
+			case 5: System.out.print("This is case 5"); break;
+		}
 	}
+
 	public static void decision(int a, b)
 	{
 		//I still don't really know what I'm doing here.
