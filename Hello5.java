94a1e996fab3923c499a702fa59b878a5be3fea8
PhilMcConn <60410681+PhilMcConn@users.noreply.github.com>
2020-02-03 16:08:42 -0600

    Add files via upload

--- Hello5.java
+++ Hello5.java
@@ -14,13 +14,33 @@ class Hello5
 		}
 		else 
 			System.out.println("Hello " + name + " nice to meet you.");
-		}
 		
-		void decision(int d)
-		{
-				System.out.println(d);
+	}
+		
+			void decision(int d){
+			switch (d) {
+			case 1:
+				System.out.println("This is case 1");
+			break;
+			case 2:
+				System.out.println("This is case 2");
+			break;
+			case 3:
+				System.out.println("This is case 3");
+			break;
+			case 4:
+				System.out.println("This is case 4");
+			break;
+			case 5:
+				System.out.println("This is case 5");
+			break;
+			
+			default: System.out.println("This is default");
+			
+			}
+			
 		}
-
+				
 		void decision(String d)
 		{
 				System.out.println(d);
@@ -28,8 +48,7 @@ class Hello5
 
 		void decision(int id ,String sd)
 		{
-				System.out.println(id"" + sd);
+				System.out.println(id+" "+sd);
 		}
-
-
+		
 }
